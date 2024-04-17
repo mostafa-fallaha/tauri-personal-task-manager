07862ed33b31d1eb2fa0a8efd0c65eb79ecaddcf
mostafa fallaha <moostafafallaha@gmail.com>
2024-04-17 17:19:11 +0300

    setting the redux state

---
+++ src/interfaces/Task.ts
@@ -0,0 +1,7 @@
+export interface Task {
+  id: number;
+  text: string;
+  task_done: boolean;
+  duration: string;
+  date_added: string;
+}
---
+++ src/state/task/taskSlice.ts
@@ -0,0 +1,72 @@
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { Task } from "../../interfaces/Task";
+import { invoke } from "@tauri-apps/api/tauri";
+
+const initialState = {
+  tasks: [] as Task[],
+};
+
+const taskSlice = createSlice({
+  name: "task",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getTasks.fulfilled, (state, action: PayloadAction<Task[]>) => {
+        state.tasks = action.payload;
+      })
+      .addCase(insertTask.fulfilled, (state, action: PayloadAction<Task>) => {
+        state.tasks = [action.payload, ...state.tasks];
+      })
+      .addCase(deleteTask.fulfilled, (state, action: PayloadAction<Task[]>) => {
+        state.tasks = action.payload;
+      })
+      .addCase(
+        setTaskStatus.fulfilled,
+        (state, action: PayloadAction<Task[]>) => {
+          state.tasks = action.payload;
+        }
+      );
+  },
+});
+
+export const getTasks = createAsyncThunk<Task[]>("task/getTasks", async () => {
+  const res = await invoke<Task[]>("get_tasks");
+  return res;
+});
+
+interface InsertInterface {
+  newText: string;
+  newDuration: string;
+}
+
+export const insertTask = createAsyncThunk<Task, InsertInterface>(
+  "task/insertTask",
+  async ({ newText, newDuration }: InsertInterface) => {
+    const res = await invoke<Task>("insert_task", { newText, newDuration });
+    return res;
+  }
+);
+
+export const deleteTask = createAsyncThunk<Task[], number>(
+  "task/deleteTask",
+  async (id: number) => {
+    const res = await invoke<Task[]>("delete_task", { id });
+    return res;
+  }
+);
+
+interface TaskInfos {
+  id: number;
+  taskDone: boolean;
+}
+
+export const setTaskStatus = createAsyncThunk<Task[], TaskInfos>(
+  "task/setTaskStatus",
+  async ({ id, taskDone }: TaskInfos) => {
+    const res = await invoke<Task[]>("set_task_status", { id, taskDone });
+    return res;
+  }
+);
+
+export default taskSlice.reducer;
