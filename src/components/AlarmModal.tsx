b070e741784a20af0ac452dff78885ddbecf6590
mostafa fallaha <moostafafallaha@gmail.com>
2024-04-17 17:24:31 +0300

    created the AlarmModal.tsx

---
+++ src/components/AlarmModal.tsx
@@ -0,0 +1,42 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  //   useDisclosure,
+  Button,
+} from "@chakra-ui/react";
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+function AlarmModal({ isOpen, onClose }: Props) {
+  //   const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      {/* <Button onClick={onOpen}>Open Modal</Button> */}
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Timeout!</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Your task time is over</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="purple" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default AlarmModal;
