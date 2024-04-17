a1a621ee816a9bc7bff58066628ebcfd1661c55e
mostafa fallaha <moostafafallaha@gmail.com>
2024-04-17 11:43:06 +0300

    setting and adding the commands to the invoke_handler in the main.rs file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,35 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+
+mod commands;
+mod db;
+mod utils;
+
+// use futures::executor::block_on;
+// use sea_orm::DatabaseConnection;
+// use tauri::{Manager, State};
+// use tokio;
+
+// pub struct AppState(DatabaseConnection);
 
 fn main() {
+    // let connection = block_on(db::get_connection()).unwrap();
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        // .manage(AppState(connection))
+        .invoke_handler(tauri::generate_handler![
+            commands::test_connection,
+            commands::get_tasks,
+            commands::insert_task,
+            commands::delete_task,
+            commands::set_task_status
+        ])
+        // .setup(|_app| {
+        //     // Initialize the database.
+        //     let _ = block_on(db::get_connection()).unwrap();
+        //     Ok(())
+        // })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
